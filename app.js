b150a78480202da13695ef6763d6ee17daa6d138
saif ali <saif7054a@gmail.com>
2024-10-07 01:56:11 +0530

    rashid ne para add kiya in app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 let button =document.createElement('button');
 button.innerText='click to change color';
 document.body.appendChild(button);
+let para=document.createElement('p');
+ para.innerText='this para is created by rashid branch';
+ document.body.appendChild(para);
